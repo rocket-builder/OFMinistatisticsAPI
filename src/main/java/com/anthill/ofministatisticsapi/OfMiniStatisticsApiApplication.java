c851c409709e6eefb1ed8b330edc0620e5cd15d7
rocket-builder <53790403+rocket-builder@users.noreply.github.com>
2022-02-16 16:35:24 +0300

    init

---
+++ src/main/java/com/anthill/ofministatisticsapi/OfMiniStatisticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.anthill.ofministatisticsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfMiniStatisticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfMiniStatisticsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anthill/ofministatisticsapi/OfMiniStatisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthill.ofministatisticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfMiniStatisticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
