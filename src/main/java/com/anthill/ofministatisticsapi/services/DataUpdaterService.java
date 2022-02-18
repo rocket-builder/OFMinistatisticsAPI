26401f483c05d2f883db410d6bfbe3d524f53312
rocket-builder <53790403+rocket-builder@users.noreply.github.com>
2022-02-18 20:02:37 +0300

    Update DataUpdaterService.java

--- src/main/java/com/anthill/ofministatisticsapi/services/DataUpdaterService.java
+++ src/main/java/com/anthill/ofministatisticsapi/services/DataUpdaterService.java
@@ -26,7 +26,7 @@ public class DataUpdaterService {
         this.telegramService = telegramService;
     }
 
-    @Scheduled(fixedDelay = 86400)
+    @Scheduled(fixedDelay = 1800)
     public void updateAllModelsStatistics() {
         log.info("start update all models statistics at "+ LocalDateTime.now());
 
