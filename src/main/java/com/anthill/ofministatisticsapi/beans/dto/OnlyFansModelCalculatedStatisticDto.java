c914b0115d03134cea10288f8a1008990c0914e4
rocket-builder <53790403+rocket-builder@users.noreply.github.com>
2022-03-14 20:08:37 +0300

    update model page statistic endpoint

---
+++ src/main/java/com/anthill/ofministatisticsapi/beans/dto/OnlyFansModelCalculatedStatisticDto.java
@@ -0,0 +1,18 @@
+package com.anthill.ofministatisticsapi.beans.dto;
+
+import com.anthill.ofministatisticsapi.beans.OnlyFansModel;
+import com.anthill.ofministatisticsapi.beans.Statistic;
+import lombok.*;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class OnlyFansModelCalculatedStatisticDto {
+
+    private OnlyFansModel model;
+    private Statistic current;
+    private List<CalculatedStatisticDto> historical;
+}
--- src/main/java/com/anthill/ofministatisticsapi/controllers/mvvm/OnlyFansModelPageController.java
+++ src/main/java/com/anthill/ofministatisticsapi/controllers/mvvm/OnlyFansModelPageController.java
@@ -1,5 +1,8 @@
 package com.anthill.ofministatisticsapi.controllers.mvvm;
 
+import com.anthill.ofministatisticsapi.beans.Statistic;
+import com.anthill.ofministatisticsapi.beans.dto.CalculatedStatisticDto;
+import com.anthill.ofministatisticsapi.beans.dto.OnlyFansModelCalculatedStatisticDto;
 import com.anthill.ofministatisticsapi.beans.dto.OnlyFansModelStatisticDto;
 import com.anthill.ofministatisticsapi.exceptions.CannotGetStatisticException;
 import com.anthill.ofministatisticsapi.exceptions.ResourceNotFoundedException;
@@ -7,7 +10,10 @@ import com.anthill.ofministatisticsapi.repos.OnlyFansModelRepos;
 import com.anthill.ofministatisticsapi.repos.StatisticRepos;
 import com.anthill.ofministatisticsapi.services.DataScrapperService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import one.util.streamex.StreamEx;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
 
 @Tag(name = "OnlyFansModelPage")
 @RequestMapping("/page/onlyFansModel")
@@ -26,7 +32,7 @@ public class OnlyFansModelPageController {
     }
 
     @GetMapping("/{id}/statistic")
-    public OnlyFansModelStatisticDto getLastDays(@PathVariable("id") long id, @RequestParam int days)
+    public OnlyFansModelCalculatedStatisticDto getLastDays(@PathVariable("id") long id, @RequestParam int days)
             throws ResourceNotFoundedException, CannotGetStatisticException {
         var modelOptional = modelRepos.findById(id);
 
@@ -35,8 +41,16 @@ public class OnlyFansModelPageController {
         }
         var model = modelOptional.get();
         var current = scrapperService.getStatistic(modelOptional.get().getUrl());
-        var historical = statisticRepos.findLastGlobalPointsByModel(id, days);
+        var historical = statisticRepos.findLastGlobalPointsByModel(id, days + 1);
 
-        return new OnlyFansModelStatisticDto(model, current, historical);
+        var calculated = StreamEx.of(historical)
+                .pairMap((p, n) ->
+                        CalculatedStatisticDto.builder()
+                                .calculated(Statistic.subtract(n, p))
+                                .statistic(n)
+                                .build())
+                .collect(Collectors.toList());
+
+        return new OnlyFansModelCalculatedStatisticDto(model, current, calculated);
     }
 }
